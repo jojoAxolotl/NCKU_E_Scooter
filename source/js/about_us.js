ed8f3878b49f304cde68ba1273920385b5934058
ShenChunWei <chunwei900824@gmail.com>
2022-04-03 11:31:44 +0800

    third commit

--- source/js/about_us.js
+++ source/js/about_us.js
@@ -1,3 +1,36 @@
-$(document).ready(function(){
-  $('#fullpage').fullpage();
+AOS.init();  // AOS initiation
+
+$('.aos-init').removeClass('aos-animate'); // remove ALL classes which triggers animation in document
+
+new fullpage('#fullpage', {
+    //.............................................
+    //....Your other options....
+    //..............................................
+    anchors: ['page1', 'page2', 'page3', 'page4'],
+    onLeave: function(){
+        jQuery('.section [data-aos]').removeClass("aos-animate");
+    },
+    onSlideLeave: function(){
+        jQuery('.slide [data-aos]').removeClass("aos-animate");
+    },
+    afterSlideLoad: function(){
+        jQuery('.slide.active [data-aos]').addClass("aos-animate");
+    },
+    afterLoad: function(){
+        jQuery('.section.active [data-aos]').addClass("aos-animate");
+       //jQuery('.fp-table.active .aos-init').addClass('aos-animate');
+    }
 });
+
+$(document).on('click', '#about_us', function(){
+  fullpage_api.moveTo('page2');
+});
+
+$(document).on('click', '#about_scooter', function(){
+  fullpage_api.moveTo('page3');
+});
+
+$(document).on('click', '#sign', function(){
+  fullpage_api.moveTo('page4');
+});
+
