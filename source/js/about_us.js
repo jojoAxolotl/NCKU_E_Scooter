ddddea8b830905d6101df0e50bbd1c9d3618d571
ShenChunWei <chunwei900824@gmail.com>
2022-04-06 22:44:57 +0800

    conflict fixed

--- source/js/about_us.js
+++ source/js/about_us.js
@@ -1,5 +1,28 @@
 AOS.init();  // AOS initiation
 
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 300, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
 $('.aos-init').removeClass('aos-animate'); // remove ALL classes which triggers animation in document
 
 new fullpage('#fullpage', {
@@ -22,6 +45,10 @@ new fullpage('#fullpage', {
     }
 });
 
+$(document).on('click', '#home', function(){
+  fullpage_api.moveTo('page1');
+});
+
 $(document).on('click', '#about_us', function(){
   fullpage_api.moveTo('page2');
 });
@@ -30,7 +57,7 @@ $(document).on('click', '#about_scooter', function(){
   fullpage_api.moveTo('page3');
 });
 
-$(document).on('click', '#sign', function(){
+$(document).on('click', '#app', function(){
   fullpage_api.moveTo('page4');
 });
 
